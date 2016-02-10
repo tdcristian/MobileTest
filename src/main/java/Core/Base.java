526a67ff883bce72c3e61a81c473c7e6ece7fb2f
tescu <tdcristian@gmail.com>
2016-02-10 12:57:12 +0200

    Update apps

--- src/main/java/Core/Base.java
+++ src/main/java/Core/Base.java
@@ -116,7 +116,7 @@ public abstract class Base {
             for (String context :contents) {
                 logger.info("context = " + context);
             }
-            Thread.sleep(1000);
+            Thread.sleep(2000);
             getDriver().context((String) contents.toArray()[1]);
             contentName = (String) contents.toArray()[1];
             logger.info("Execute: switchContentToWeb("+contents.toArray()[1]+")");
